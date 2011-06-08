a1a4fb4ab14e5aee9fcddf89b502b49fad1099bf
SeB <essembeh@gmail.com>
2011-06-08 22:20:55 +0200

    Adding symlinks support for startup scripts

--- bin/start_macos.sh
+++ bin/start_macos.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+RTFM_BIN=$0
+while [ -L "$RTFM_BIN" ]; do
+	RTFM_BIN="$(dirname "$RTFM_BIN")/$(readlink "$RTFM_BIN")"
+	echo "RTFM_BIN=$RTFM_BIN"
+done
+
 JAVA=$(which java)
 RTFM_MAINCLASS="org.essembeh.rtfm.starter.MainClass"
-RTFM_APP="$(dirname $0)/../"
+RTFM_APP="$(dirname "$RTFM_BIN")/../"
 RTFM_HOME="$HOME/.rtfm/"
 
 ## configuration folder in user home folder
