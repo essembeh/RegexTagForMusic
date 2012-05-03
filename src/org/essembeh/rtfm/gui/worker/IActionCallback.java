19da523cb41f9f953e73c69450ece41d1f5a6323
Sébastien M-B <essembeh@gmail.com>
2012-05-03 16:16:13 +0200

    Adding header to files

--- src/org/essembeh/rtfm/gui/worker/IActionCallback.java
+++ src/org/essembeh/rtfm/gui/worker/IActionCallback.java
@@ -1,3 +1,22 @@
+/**
+ * Copyright 2011 Sebastien M-B <essembeh@gmail.com>
+ * 
+ * This file is part of RegexTagForMusic.
+ * 
+ * RegexTagForMusic is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * RegexTagForMusic is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * RegexTagForMusic. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
 package org.essembeh.rtfm.gui.worker;
 
 import org.essembeh.rtfm.core.exception.ActionException;
